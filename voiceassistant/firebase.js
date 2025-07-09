c8156550bac4cb545aba516930fd204714f87ba3
Sumit Samal <sumitsamal08@gmail.com>
2025-07-09 13:01:53 +0530

    commit

--- voiceassistant/firebase.js
+++ voiceassistant/firebase.js
@@ -1,6 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
-
 const firebaseConfig = {
   apiKey: "AIzaSyA7N3gbwM2b249hIYlcwzzzsQmmen55A4U",
   authDomain: "voice-assistant-9f259.firebaseapp.com",
@@ -10,9 +7,3 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
-
-const app = initializeApp(firebaseConfig);
-const appCheck = getAppCheck(app, {
-  provider: new ReCaptchaV3Provider("6Lcq-3wrAAAAANZIK2_JsMyOcQABaClQW5_e4uKH"),
-  isTokenAutoRefreshEnabled: true,
-});
